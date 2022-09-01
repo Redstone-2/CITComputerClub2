887a1501d203add97abd8091dc5aa41ab006c696
Mrredstone2 <87745349+Mrredstone2@users.noreply.github.com>
2022-09-01 15:17:52 -0400

    Create usrsignup.js

---
+++ helpers/js/usrsignup.js
@@ -0,0 +1,56 @@
+document.getElementById("my_button").addEventListener("click", makeUsr);
+
+function makeUsr(ev){
+  ev.preventDefault(); 
+  ev.stopImmediatePropagation(); 
+  
+var form = document.getElementById("loginForm");
+var fname = document.getElementById("fname").value;
+var lname = document.getElementById("lname").value; 
+var pswor = document.getElementById("pswor").value;
+var email = document.getElementById("emaile").value;
+  
+ var checkBox = document.getElementById('cshs');
+ if (checkBox.checked == true){
+   var cshs = "ADMIN";
+ } else {
+   var cshs = "USER";
+ }
+  
+var checkBox = document.getElementById('admin');
+ if (checkBox.checked == true){
+   var admin = "CSHS_Member";
+ } else {
+   var admin = "Not_CSHS";
+ }
+ 
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+ 
+var raw = JSON.stringify({
+  "First_name": fname,
+  "Last_name": lname,
+  "Password": pswor,
+  "Email": email,
+  "User_type": admin,
+  "CSHS": cshs
+});
+ 
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+ 
+fetch("https://computerclub.duckdns.org/admin/signup", requestOptions)
+  .then(async response =>  {console.log(response);
+       var result = response.headers.get('content-type')?.includes('application/json') ? await response.json() : null;
+        if (result.error == null){
+          alerts("Success");
+        } else {
+          alerts(result.error);
+        }
+   })
+  .catch(error => alerts(error))
+}
